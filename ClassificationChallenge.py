a38b36d89146cf1e2ae747867962d0280786eba7
GeovanniOj <geovannirock@gmail.com>
2019-01-31 17:21:00 -0200

    Different classifiers were tested

--- ClassificationChallenge.py
+++ ClassificationChallenge.py
@@ -1,11 +1,9 @@
 import scipy
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas.plotting import scatter_matrix
 from sklearn import model_selection
-from sklearn.impute import SimpleImputer
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -13,18 +11,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 #load data GPAM_Challenge dataset 
 dataset = pd.read_csv('olist_order_reviews_dataset.csv') 
 dataset = dataset.fillna('')
 
-print(dataset.shape)
-print(dataset.head(10))
-print(dataset.describe())
-print(dataset.groupby('review_score').size())
+# print(dataset.shape)
+# print(dataset.head(10))
+# print(dataset.describe())
+# print(dataset.groupby('review_score').size())
 
 #check data distribution
 # dataset.hist()
@@ -39,6 +37,7 @@ X = array[:,4]
 
 #split rating column
 Y = array[:,2]
+Y = Y.astype(int)
 
 #amount in percentage of the data to be put into the validation set
 validation_size = 0.3
@@ -48,18 +47,29 @@ seed = 5
 
 # Vectorizer to split text
 vectorizer = TfidfVectorizer(ngram_range=(1,2))
-# x_vectorized = vectorizer.fit_transform(X)
+x_vectorized = vectorizer.fit_transform(X)
 
 #split the datset between train and validation
-# X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X,
-#                                                 Y,
-#                                                 test_size=validation_size,
-#                                                 random_state=seed)
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(x_vectorized,
+                                                Y,
+                                                test_size=validation_size,
+                                                random_state=seed)
 
-# print("Training set has {} samples.".format(X_train.shape[0]))
-# print("Test set has {} samples.".format(X_validation.shape[0]))
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Test set has {} samples.".format(X_validation.shape[0]))
+
+#Classifier
+linearClassificer = LinearSVC()
+linearClassificer.fit(X_train, Y_train)
+linearPrediction = linearClassificer.predict(X_validation)
+print(accuracy_score(Y_validation,linearPrediction))
 
 # classifierKNN = KNeighborsClassifier(n_neighbors=2)
 # classifierKNN.fit(X_train, Y_train)
 # prediction = classifierKNN.predict(X_validation)
 # print("KNN:", accuracy_score(Y_validation, prediction))
+
+multinomialClassifier = MultinomialNB()
+multinomialClassifier.fit(X_train, Y_train)
+prediction = multinomialClassifier.predict(X_validation)
+print(accuracy_score(Y_validation, prediction))
